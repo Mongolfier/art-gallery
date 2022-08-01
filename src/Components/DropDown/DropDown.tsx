d5e16940e71cbef2aa8e4f8ae47342db4a837db7
Dmitrij Mumrenkov <dmumrenkov@getmobit.ru>
2022-08-01 16:29:10 +0300

    разъеб

--- src/Components/DropDown/DropDown.tsx
+++ src/Components/DropDown/DropDown.tsx
@@ -4,22 +4,44 @@ import DropDownItem from "./DropDownItem";
 import { Fragment, useState } from "react";
 
 interface DropDownProps {
-  children: JSX.Element[] | string[];
+  // children: JSX.Element[] | string[];
+  items: DropdownItems[];
+  // components?: {
+  //   item?: ComponentType<DropdownItemProps>,
+  // }
   label: string;
 }
 
 const DropDown = ({children, label}: DropDownProps) => {
-  
+
   const [isOpen, setIsOpen] = useState(false);
 
   const handlerToggle = () => {
     setIsOpen(!isOpen);
   }
 
+  const children = React.Children.toArray(children);
+
+  for (const child of children) {
+    switch (child.type) {
+      case DropDown.Toggle: {
+        const clone = React.cloneElement(child);
+        clone.props.onClick = handlerToggle;
+        break;
+      }
+      case DropDown.Menu: {
+        const clone = React.cloneElement(child);
+        clone.props.onClick = handlerToggle;
+
+        break;
+      }
+  }
+
   return (
   <>
-    <DropDownToggle onClick={handlerToggle} >{label}</DropDownToggle>
-    <Fragment isOpen={isOpen}>{children}</Fragment>
+    {clone}
+    {/* <DropDownToggle onClick={handlerToggle} >{label}</DropDownToggle>
+    <Fragment isOpen={isOpen}>{children}</Fragment> */}
   </>
   );
 }
--- src/Components/DropDown/DropDownItem.tsx
+++ src/Components/DropDown/DropDownItem.tsx
@@ -1,3 +1,5 @@
+import DropDown from "./DropDown";
+
 interface DropDownItemProps {
   children: JSX.Element | React.FC | string;
 }
@@ -7,3 +9,12 @@ const DropDownItem = ({children}: DropDownItemProps): JSX.Element => {
 }
 
 export default DropDownItem;
+
+<DropDown>
+  <DropDown.Toggle /> { type: DropDown.Toggle, props: {}} => { type: DropDown.Toggle, props: { onClick}}
+  <DropDown.Menu>
+    <DropDown.Item></DropDown.Item>
+    <DropDown.Item></DropDown.Item>
+    <DropDown.Item></DropDown.Item>
+  </DropDown.Menu>
+</DropDown>
